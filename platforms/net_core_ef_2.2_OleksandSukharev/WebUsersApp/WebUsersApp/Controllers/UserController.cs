de2ebd3f529fd9dbb1d7c04c8553768ef25b1916
Oleksander Sukharev <sambademon@gmail.com>
2019-03-26 00:20:59 +0200

    Add v1 .netCore + EFCore + Razor

---
+++ platforms/net_core_ef_2.2_OleksandSukharev/WebUsersApp/WebUsersApp/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebUsersApp.Controllers
+{
+    using Models.Context;
+    using Models.Domain;
+
+    public class UserController : Controller
+    {
+        private UserContext _userContext;
+
+        private static List<User> _users = new List<User>
+        {
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+            new User() {FirstName ="Lola", LastName="Samara", Age=27, Email="lola.samara27@example.com", EducationId = 0, GenderId = 0, Education = new Education{Id = 0, Name = "High"}, Gender = new Gender{Id=0,Name = "Female"}},
+        };
+
+        public UserController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        [HttpGet]
+        [Route("empty")]
+        public IActionResult GetEmpty() => Ok();
+
+        [HttpGet]
+        [Route("users")]
+        public IActionResult GetStaticUser() => View("UsersStatic");
+
+        [HttpGet]
+        [Route("mock-users")]
+        public IActionResult GetMockUserAsync() => View("Users", _users);
+
+        [HttpGet]
+        [Route("db-users")]
+        public async Task<IActionResult> GetDbUserAsync() => View("Users", await _userContext.Users.ToListAsync());
+    }
+}
